7f50673c3627c85530fe96e559c7002666b8a267
sinchana-km <sinchana1012003@gmail.com>
2026-08-16 22:12:22 +0530

    hackerrank problem updated

---
+++ 02_arithmatic operators.py
@@ -0,0 +1,6 @@
+a = int(input("Enter num a:"))
+b = int(input("Enter num b:"))
+
+print(a + b)
+print(a - b)
+print(a * b)
---
+++ 03_ifelifelse.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+if(n %2!=0 ):
+    print("Weird")
+    
+elif(n %2==0 and 2<=n<=5 ):
+    print("Not weird")
+    
+elif(n %2==0 and 6<=n<=20):
+    print("Weird")
+    
+else:
+    print("Not Weird")        
+    
